377383626e1d7ccb965cc400926f87af1b68e9d1
linhng12 <linh.nguyen@wizeline.com>
2025-11-07 16:12:07 +0700

    update init

--- number_tools/__init__.py
+++ number_tools/__init__.py
@@ -9,7 +9,6 @@ from .arithmetic import (
     multiply,
     subtract,
 )
-from .scaling import min_max_scale, normalize, rescale, scale_linear, standardize
 from .series import NumberSeries, arithmetic_sequence, fibonacci, geometric_sequence
 from .statistics import SummaryStatistics, mean, median, mode, summarize, variance
 
@@ -21,11 +20,6 @@ __all__ = [
     "factorial",
     "get_square_root",
     "is_positive_number",
-    "normalize",
-    "scale_linear",
-    "rescale",
-    "standardize",
-    "min_max_scale",
     "NumberSeries",
     "fibonacci",
     "arithmetic_sequence",
